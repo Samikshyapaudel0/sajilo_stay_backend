ecd2d4616a7a771f6e3d84b1954f367052dffff8
Samikshyapaudel0 <samikshyapaudel61@gmail.com>
2026-06-28 07:32:32 +0545

    added admin routes

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -12,6 +12,13 @@ userRouter.post("/login", userController.loginUser);
 userRouter.get("/whoami", authorizedMiddleware, userController.whoami);
 
 userRouter.put(
+  "/update-password",
+  authorizedMiddleware, // handle authentication and set req.user
+  uploads.single("profileImage"), // handle profile image upload
+  userController.updateUser,
+);
+
+userRouter.put(
   "/update",
   authorizedMiddleware, // handle authentication and set req.user
   uploads.single("profileImage"), // handle profile image upload
